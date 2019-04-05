39ace346711a49736781724644451eb5cd631146
Becca Lynch <lynchre@umich.edu>
2019-04-04 23:17:58 -0500

    more new candidate script updtaes

--- scripts/new_cand.py
+++ scripts/new_cand.py
@@ -1,4 +1,24 @@
 import sys
 
-if __name__ == "__main__":
+def insert_cand(cname):
+    # insert candidate into candidate table
+    cname = cname.split(' ')
+    if len(cname) != 2:
+        print("Error: candidate name must be <first> <last>")
+        sys.exit(1)
 
+    q_string = """
+        INSERT INTO query_dup
+        (cid, first_name, middle_name, last_name, delegate_count, nickname, img, color)
+        VALUES
+        (NULL, "{}", NULL, "{}", 0, NULL, NULL, NULL);
+    """.format(cname[0], cname[1])
+
+    print("Executing...\n{}".format(q_string))
+
+if __name__ == "__main__":
+    try:
+        cname = sys.argv[1]
+    except:
+        print("Error: please pass in a candidate name")
+        sys.exit(1)
