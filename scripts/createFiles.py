c7f43b87d3ebdac16349181ac16e878aa89d7351
Dang Nguyen <nhhdang@gmail.com>
2019-04-06 22:12:09 -0700

    fix candidate summary data generator

--- scripts/createFiles.py
+++ scripts/createFiles.py
@@ -5,6 +5,8 @@ import mysql.connector as mariadb
 
 mariadb_connection = mariadb.connect(user='root', password='datapult49', database='gtep_test')
 cursor = mariadb_connection.cursor()
+targetFile = "candidate_summary.tsv"
+targetPath = "/opt/tomcat/webapps/sample/data/"
 
 def create_delegate_file():
     """
@@ -29,37 +31,44 @@ def create_delegate_file():
     # Parse and transform into list.
     delegateLine_list = []
     for tup in result:
-        delegateLine_list.append(["{}".format(tup[0]), "{}".format(tup[1]), 
-                             "{}".format(tup[2]), "{}".format(tup[3]),
-                             "{}".format(tup[4])])
-    
+        delegateLine_list.append([tup[1], tup[2], tup[3], tup[4]])
+  
     # Convert to pandas dataframes
     delegateLine = pandas.DataFrame.from_records(delegateLine_list)
     
     if delegateLine.empty:
         print('Summary table was empty, not writing over delegateLine.csv')
     else:
-        delegateLine.columns = ['cid', 'first_name', 'last_name', 'sdate', 'amount']
-        delegateLine['cid'] = delegateLine['cid'].astype(int)
+        delegateLine.columns = ['first_name', 'last_name', 'sdate', 'amount']
         delegateLine['first_name'] = delegateLine['first_name'].astype(str)
         delegateLine['last_name'] = delegateLine['last_name'].astype(str)
         delegateLine['sdate'] = delegateLine['sdate'].astype(str)
-        delegateLine['amount'] = delegateLine['amount'].astype(int)
+
         # Now I need to change the format to match what is needed
         # Need columns as date and then name of each candidate
-        # Need date as YMD like 20111001
         delegateLine['name'] = delegateLine[['first_name', 'last_name']].apply(
                 lambda x: ' '.join(x), axis=1)
         del delegateLine['first_name']
         del delegateLine['last_name']
-        del delegateLine['cid']
-        delegateLine = delegateLine.pivot(index='sdate', columns='name')
+        delegateLine = delegateLine.pivot(index='sdate', columns='name', values='amount')
         delegateLine.reset_index(level=0, inplace=True)
+        # Need date as YMD like 20111001
         delegateLine['sdate'] = delegateLine['sdate'].astype(str)
         delegateLine.sdate = delegateLine.sdate.str.slice(0,10)
-        return delegateLine
-        #delegateLine.to_csv('./trending2020/data/candidate_summary.tsv', sep='\t', encoding='utf-8')
-        #print('Updated delegateLine.csv')
+        delegateLine.sdate = delegateLine.sdate.str.replace("-","")
+        # Capitalize Candidate Name
+        delegateLine.columns = map(str.title, delegateLine.columns)
+        # Rename the first column to match requirement for ploting function
+        delegateLine.rename(columns={'Sdate': 'date'}, inplace=True)
+
+        # To return calculated dataframe, uncomment the line below
+        # return delegateLine
+        
+        # After clean data i.e. remove unwanted row/col, use right header to match required format of plotting fucntion
+        # Write to TSV file
+        delegateLine.to_csv(targetPath+targetFile, sep='\t', encoding='utf-8', index=False)
+
+        print('--Updated ' + targetFile)
     
 def write_all():
     create_delegate_file()
